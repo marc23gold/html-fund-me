6a4cf3f93007eff02339c81af29a6ac42da3e3f4
Marcx <haitianhero6@gmail.com>
2022-10-06 10:22:34 -0400

    fund me function

--- index.js
+++ index.js
@@ -1,12 +1,70 @@
+import { ethers } from "./ethers-5.6.esm.min.js";
+
+import { abi, contractAddress } from "./constants.js";
+
+const connectButton = document.getElementById("connectButton");
+const fundButton = document.getElementById("fundButton");
+connectButton.onclick = connect;
+fundButton.onclick = fund;
+
+console.log(ethers);
+
 async function connect() {
   console.log("Hi, I'm from script tage");
   if (typeof window.ethereum !== "undefined") {
-    await window.ethereum.request({ method: "eth_requestAccounts" });
-    console.log("Connected");
-    document.getElementById("connectButton").innerHTML = "Connected!";
+    try {
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+    } catch (error) {
+      console.log(error);
+    }
+    connectButton.innerHTML = "Connected!";
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    console.log(accounts);
   } else {
     console.log("No metamask");
-    document.getElementById("connectButton").innerHTML =
-      "Please Install metamask";
+    connectButton.innerHTML = "Please Install metamask";
   }
+}
+
+async function fund() {
+  const ethAmount = "77";
+  console.log(`Funding with ${ethAmount}...`);
+  if (typeof window.ethereum !== "undefined") {
+    //provider/connection to blockchain
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    //signer/wallet/someone with some gas
+    const signer = provider.getSigner();
+    //console.log(signer);
+    //contract that we are interacting with ABI and Address
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+      const transactionResponse = await contract.fund({
+        value: ethers.utils.parseEther(ethAmount),
+      });
+      await listenForTransactionMine(transactionResponse, provider);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    fundButton.innerHTML("Please install metamask");
+  }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+  console.log(`Mining ${transactionResponse.hash}...`);
+  //return new Promise();
+  //create a listener for the blockchain
+  //listen for transaction to finish
+  return new Promise((resolve, reject) => {
+    provider.once(transactionResponse.hash, (transactionReciept) => {
+      console.log(
+        `Completed with ${transactionReciept.confirmations} confirmations`
+      );
+      resolve();
+    });
+  });
+}
+
+async function withdraw() {
+  console.log(`Withdrawing`);
 }
